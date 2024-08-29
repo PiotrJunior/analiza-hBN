ee5742e1a9cecaa3fff17c182c1e418945ed4e7c
Piotr Maksymiuk <pm449319@students.mimuw.edu.pl>
2024-08-29 21:16:14 +0200

    Nowe PCA

--- notebooks/PCA.py
+++ notebooks/PCA.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy import dot
-from numpy.linalg import eigh, eig
+from numpy.linalg import eigh, eig, norm, inv
 import pandas as pd
 import plotly.express as px
 from plotly import graph_objects as go
@@ -35,19 +35,31 @@ def line(X, offset):
     return np.ones(len(X)) * offset
 
 
-# def both_lorentz(X, mean1, sigma1, scale1, offset1, mean2, sigma2, scale2, offset2):
-#     return lorentz(X, mean1, sigma1, scale1, offset1) + lorentz_shift(X, mean2, sigma2, scale2, offset2)
-
 class Map:
-    def __init__(self, src, x, y, **kwargs):
-        raw_data = np.loadtxt(src, delimiter='\t')
+    refit_vectors = 0
+    refit_matrices = []
+
+    def __init__(self, src, **kwargs):
+        # raw_data = np.loadtxt(src, delimiter='\t')
+        # if 'begin' in kwargs and 'end' in kwargs:
+        #     raw_data = raw_data[np.r_[0:1, (kwargs['begin'] + 2):(kwargs['end'] + 2)]]
+        # self.shape = ( len(np.where( raw_data[0][2:] == 0)), len(np.where( raw_data[1][2:] == 0)) )
+        # print(self.shape)
+        # self.raman_shift = raw_data.T[0][2:]
+        # self.data_matrix = raw_data[2:].T[1:]
+        # # self.data = pd.DataFrame(raw_data[2:], columns=['RamanShift'] + list(np.arange(0, x * y, 1)))
+        # self.map = np.array([raw_data[0][1:], raw_data[1][1:]]).T
+
+        data = pd.read_csv(src, delimiter='\t', names=['x', 'y', 'RamanShift', 'Intensity'])
         if 'begin' in kwargs and 'end' in kwargs:
-            raw_data = raw_data[np.r_[0:1, (kwargs['begin'] + 2):(kwargs['end'] + 2)]]
-        self.shape = (x, y)
-        self.raman_shift = raw_data.T[0][2:]
-        self.data_matrix = raw_data[2:].T[1:]
-        self.data = pd.DataFrame(raw_data[2:], columns=['RamanShift'] + list(np.arange(0, x * y, 1)))
-        self.map = np.array([raw_data[0][1:], raw_data[1][1:]]).T
+            data = data[(data['RamanShift'] >= kwargs['begin']) & (data['RamanShift'] <= kwargs['end'])]
+
+        self.raman_shift = data[(data['x'] == 0) & (data['y'] == 0)]['RamanShift'].values
+        print(data.head())
+        print(self.raman_shift, len(self.raman_shift), type(self.raman_shift))
+        self.shape = (len(data['x'].unique()), len(data['y'].unique()))
+        print(self.shape)
+        self.data_matrix = data['Intensity'].values.reshape((len(self.raman_shift), -1))
 
         self.cov_matrix = np.cov(self.data_matrix)
 
@@ -61,85 +73,96 @@ class Map:
     def reconstruct(self, count):
         return np.dot(self.vectors[:count].T, self.base_vectors[:count])
 
-    def plotComponent(self, num):
-        fig = px.imshow(np.reshape(self.vectors[num], self.shape), title='Komponent ' + str(num))
+    def reftit_matrix(self):
+        rm = np.identity(self.refit_vectors)
+        for refit_matrix in self.refit_matrices:
+            rm = np.dot(rm, refit_matrix)
+        return rm
+
+    def physical_base_vectors(self):
+        return np.dot(self.reftit_matrix(), self.base_vectors[self.refit_vectors])
+
+    def physical_vectors(self):
+        return np.dot(self.vectors[self.refit_vectors].T, inv(self.reftit_matrix())).T
+
+    def plotComponent(self, num, physical=False):
+        img = self.vectors[num]
+        if physical:
+            img = self.physical_vectors()[num]
+        fig = px.imshow(np.reshape(img, self.shape), title='Component ' + str(num))
         return fig
 
-    def plotPhysicalComponent(self, v):
-        img = np.dot(np.array(v), self.vectors[:len(v)])
-        fig = px.imshow(np.reshape(img, self.shape), title='Peak')
+    def plotBaseVector(self, num, physical=False, normalize=False):
+        vector = self.base_vectors[num]
+        if physical:
+            vector = self.physical_base_vectors()[num]
+        if normalize:
+            return px.line(x=self.raman_shift, y=norm(vector), title='Num: ' + str(num))
+        else:
+            return px.line(x=self.raman_shift, y=vector, title='Num: ' + str(num))
+
+    def plotSpectrums(self, *args, **kwargs):
+        fig = go.Figure()
+        for num in args:
+            fig.add_scatter(x=self.raman_shift, y=self.data_matrix[args], mode='lines', name='ID: ' + str(num), **kwargs)
+        fig.update_layout(hovermode="x")
         return fig
+
+    def plotBaseVectors(self, count, normalize=False, **kwargs):
+        fig = go.Figure()
+        for num in range(count):
+            if normalize:
+                fig.add_scatter(x=self.raman_shift, y=norm(self.base_vectors[num]),
+                                mode='lines', name='Num: ' + str(num), **kwargs)
+            else:
+                fig.add_scatter(x=self.raman_shift, y=self.base_vectors[num],
+                                mode='lines', name='Num: ' + str(num), **kwargs)
+        fig.update_layout(hovermode="x")
+        return fig
+
+    def posToId(self, x, y):
+        return self.shape[0] * y + x
 
     def plotCovMatrix(self):
         fig = px.imshow(self.cov_matrix, title='Macierz kowariancji')
         return fig
 
-    def plotWeights(self, cum, log):
+    def plotWeights(self, cum=False, log=False):
         if cum:
-            fig = px.bar(np.cumsum(self.weights), title='Wagi', log_y=log)
+            return px.bar(np.cumsum(self.weights), title='Wagi', log_y=log)
         else:
-            fig = px.bar(self.weights, title='Wagi', log_y=log)
-        return fig
+            return px.bar(self.weights, title='Wagi', log_y=log)
 
-    def plotSpectrums(self, ids, **kwargs):
-        fig = go.Figure()
-        if 'base' not in kwargs:
-            for num in ids:
-                fig.add_scatter(x=self.raman_shift, y=self.data_matrix[num], mode='lines', name='ID: ' + str(num))
-        else:
-            r = self.reconstruct(kwargs['base'])
-            for num in ids:
-                fig.add_scatter(x=self.raman_shift, y=r[num], mode='lines', name='ID: ' + str(num))
-        fig.update_layout(hovermode="x")
-        return fig
+    # def locToId(self, x, y):
+    #     return np.argmin(list(map(lambda e: np.linalg.norm([x, y] - e), self.map)))
 
-    def plotSpectrum(self, num, **kwargs):
-        return self.plotSpectrums([num], **kwargs)
-
-    def plotBaseVectors(self, count):
-        fig = go.Figure()
-        for num in range(count):
-            fig.add_scatter(x=self.raman_shift, y=self.base_vectors[num], mode='lines', name='Num: ' + str(num))
-        fig.update_layout(hovermode="x")
-        return fig
-
-    def locToId(self, x, y):
-        return np.argmin(list(map(lambda e: np.linalg.norm([x, y] - e), self.map)))
-
-    def posToId(self, x, y):
-        return self.shape[0] * y + x
-
-    def draw_curve(self, curve, params, a, b, **draw):
-        fig = go.Figure()
-        if 'spectrum' in draw:
-            fig.add_scatter(x=self.raman_shift, y=self.data_matrix[draw['spectrum']], mode='lines')
-        if 'base_vector' in draw:
-            fig.add_scatter(x=self.raman_shift, y=self.base_vectors[draw['base_vector']], mode='lines')
-        data_slice = self.data[(self.data['RamanShift'] >= a) & (self.data['RamanShift'] <= b)]['RamanShift']
-        fig.add_scatter(mode='lines', x=data_slice, y=curve(data_slice, *params))
-        fig.update_layout(hovermode="x")
-        return fig
-
-    def fit_curve(self, curve, p0, a, b, seek, **plot):
-        y = 0
-        if 'spectrum' in plot:
-            y = self.data_matrix[plot['spectrum'], (self.data['RamanShift'] >= a) & (self.data['RamanShift'] <= b)]
-        if 'base_vector' in plot:
-            y = self.base_vectors[plot['base_vector'], (self.data['RamanShift'] >= a) & (self.data['RamanShift'] <= b)]
-        data_slice = self.data[(self.data['RamanShift'] >= a) & (self.data['RamanShift'] <= b)]['RamanShift']
-
-        bounds = 0
-        if 'bounds' in plot and plot['bounds'] == 'inf':
-            bounds = ((-np.inf, -np.inf, -np.inf, -np.inf), (np.inf, np.inf, np.inf, np.inf))
-        else:
-            bounds = ((p0[0] - 2, -np.inf, -np.inf, -np.inf), (p0[0] + 2, np.inf, np.inf, np.inf))
-
-        param, cov = curve_fit(curve, data_slice, y, p0=p0, bounds=bounds)
-        print(param)
-        if 'draw' in plot and plot['draw'] == True:
-            self.draw_curve(curve, param, a, b, **plot).show()
-        return param[seek]
-
-
-# PCA analysis of raman spectrums of hBN structures with different isotope content /
-# Analiza PCA widma Ramanowskiego struktur hBN z różnymi izotpoami hBNu
+    # def draw_curve(self, curve, params, a, b, **draw):
+    #     fig = go.Figure()
+    #     if 'spectrum' in draw:
+    #         fig.add_scatter(x=self.raman_shift, y=self.data_matrix[draw['spectrum']], mode='lines')
+    #     if 'base_vector' in draw:
+    #         fig.add_scatter(x=self.raman_shift, y=self.base_vectors[draw['base_vector']], mode='lines')
+    #     data_slice = self.data[(self.data['RamanShift'] >= a) & (self.data['RamanShift'] <= b)]['RamanShift']
+    #     fig.add_scatter(mode='lines', x=data_slice, y=curve(data_slice, *params))
+    #     fig.update_layout(hovermode="x")
+    #     return fig
+    #
+    # def fit_curve(self, curve, p0, a, b, seek, **plot):
+    #     y = 0
+    #     if 'spectrum' in plot:
+    #         y = self.data_matrix[plot['spectrum'], (self.data['RamanShift'] >= a) & (self.data['RamanShift'] <= b)]
+    #     if 'base_vector' in plot:
+    #         y = self.base_vectors[plot['base_vector'], (self.data['RamanShift'] >= a) & (self.data['RamanShift'] <= b)]
+    #     data_slice = self.data[(self.data['RamanShift'] >= a) & (self.data['RamanShift'] <= b)]['RamanShift']
+    #
+    #     bounds = 0
+    #     if 'bounds' in plot and plot['bounds'] == 'inf':
+    #         bounds = ((-np.inf, -np.inf, -np.inf, -np.inf), (np.inf, np.inf, np.inf, np.inf))
+    #     else:
+    #         bounds = ((p0[0] - 2, -np.inf, -np.inf, -np.inf), (p0[0] + 2, np.inf, np.inf, np.inf))
+    #
+    #     param, cov = curve_fit(curve, data_slice, y, p0=p0, bounds=bounds)
+    #     print(param)
+    #     if 'draw' in plot and plot['draw'] == True:
+    #         self.draw_curve(curve, param, a, b, **plot).show()
+    #     return param[seek]
